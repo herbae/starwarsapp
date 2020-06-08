f4321d76b14697ef3cb67fa2fab0ec77e723713f
Iury Lazoski <lazoski@gmail.com>
2020-06-08 04:23:37 -0300

    fetching opening crawl

--- src/app/pages/film-details/film-details.page.ts
+++ src/app/pages/film-details/film-details.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-film-details',
@@ -8,12 +9,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FilmDetailsPage implements OnInit {
 
-  filmId = null;
+  film: any;
  
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
  
   ngOnInit() {
-    this.filmId = this.activatedRoute.snapshot.paramMap.get('id');
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.http.get(`https://swapi.dev/api/films/${id}`).subscribe(res => {
+      this.film = res;
+    });
   }
 
 }
--- src/app/pages/films/films.page.ts
+++ src/app/pages/films/films.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-films',
@@ -9,19 +11,17 @@ import { Router } from '@angular/router';
 })
 export class FilmsPage implements OnInit {
 
-  constructor(private navController: NavController, private router: Router) { }
+  films: Observable<any>;
+
+  constructor(private router: Router, private http: HttpClient) { }
  
   ngOnInit() {
+    this.films = this.http.get('https://swapi.dev/api/films');
   }
- 
-  openDetails() {
-    // Both of these would work!
-    // But the standard Router is recommended.
-    // this.navController.navigateForward(`/tabs/films/42`);
-    this.router.navigateByUrl(`/tabs/films/42`);
-  }
- 
-  goToPlanets() {
-    this.navController.navigateRoot(`/tabs/planets`)
+
+  openDetails(film) {
+    let split = film.url.split('/');
+    let filmId = split[split.length-2];
+    this.router.navigateByUrl(`/tabs/films/${filmId}`);
   }
 }
