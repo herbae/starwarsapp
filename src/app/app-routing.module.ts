bb12f766617bc96dd061f78bcbe28e0f0ab883e3
Iury Lazoski <lazoski@gmail.com>
2020-06-08 03:42:14 -0300

    lesson 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,26 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
+  },
+  {
+    path: 'people',
+    loadChildren: () => import('./pages/people/people.module').then( m => m.PeoplePageModule)
+  },
+  {
+    path: 'planets',
+    loadChildren: () => import('./pages/planets/planets.module').then( m => m.PlanetsPageModule)
+  },
+  {
+    path: 'films',
+    loadChildren: () => import('./pages/films/films.module').then( m => m.FilmsPageModule)
+  },
+  {
+    path: 'film-details',
+    loadChildren: () => import('./pages/film-details/film-details.module').then( m => m.FilmDetailsPageModule)
+  },
 ];
 
 @NgModule({
--- src/app/pages/people/people.page.spec.ts
+++ src/app/pages/people/people.page.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import { PeoplePage } from './people.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('PeoplePage', () => {
+  let component: PeoplePage;
+  let fixture: ComponentFixture<PeoplePage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ PeoplePage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(PeoplePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
