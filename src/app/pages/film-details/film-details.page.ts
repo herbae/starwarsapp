6012780966c5b0cfbb431f555e09407015caa682
Iury Lazoski <lazoski@gmail.com>
2020-06-09 01:18:11 -0300

    favorites

--- src/app/pages/film-details/film-details.page.ts
+++ src/app/pages/film-details/film-details.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 import { EmailComposer } from '@ionic-native/email-composer/ngx';
+import { FavoriteService } from 'src/app/services/favorite.service';
 
 @Component({
   selector: 'app-film-details',
@@ -11,16 +12,35 @@ import { EmailComposer } from '@ionic-native/email-composer/ngx';
 export class FilmDetailsPage implements OnInit {
 
   film: any;
+  isFavorite = false;
+  filmId = null;
  
   constructor(private activatedRoute: ActivatedRoute, private api: ApiService,
-    private emailComposer: EmailComposer) { }
+    private emailComposer: EmailComposer, private favoriteService: FavoriteService) { }
  
   ngOnInit() {
-    let id = this.activatedRoute.snapshot.paramMap.get('id');
-    this.api.getFilm(id).subscribe(res => {
+    this.filmId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.api.getFilm(this.filmId).subscribe(res => {
       this.film = res;
     });
+
+    this.favoriteService.isFavorite(this.filmId).then(isFav => {
+      this.isFavorite = isFav;
+    });
   }
+
+  favoriteFilm() {
+    this.favoriteService.favoriteFilm(this.filmId).then(() => {
+      this.isFavorite = true;
+    });
+  }
+
+  unfavoriteFilm() {
+    this.favoriteService.unfavoriteFilm(this.filmId).then(() => {
+      this.isFavorite = false;
+    });
+  }
+
 
   shareFilm() {
     let email = {
