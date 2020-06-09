b7f96146683dd65e71e836c24ff1a247a1b3fd54
Iury Lazoski <lazoski@gmail.com>
2020-06-09 00:33:13 -0300

    email plugin

--- src/app/pages/film-details/film-details.page.ts
+++ src/app/pages/film-details/film-details.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
 
 @Component({
   selector: 'app-film-details',
@@ -11,13 +12,25 @@ export class FilmDetailsPage implements OnInit {
 
   film: any;
  
-  constructor(private activatedRoute: ActivatedRoute, private api: ApiService) { }
+  constructor(private activatedRoute: ActivatedRoute, private api: ApiService,
+    private emailComposer: EmailComposer) { }
  
   ngOnInit() {
     let id = this.activatedRoute.snapshot.paramMap.get('id');
     this.api.getFilm(id).subscribe(res => {
       this.film = res;
     });
+  }
+
+  shareFilm() {
+    let email = {
+      to: 'lazoski@gmail.com',
+      subject: `I love this one: ${this.film.title}`,
+      body: `Can you remember the opening?<br><br>"${this.film.opening_crawl}"`,
+      isHtml: true
+    };
+
+    this.emailComposer.open(email);
   }
 
 }
